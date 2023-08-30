a89c47f0ad90b24cceb94b18d4d67ead693f7424
sinedied <noda@free.fr>
2023-08-30 16:13:25 +0200

    feat: add config service

--- packages/api/src/plugins/config.ts
+++ packages/api/src/plugins/config.ts
@@ -1,44 +1,61 @@
-import process from "node:process";
-import path from "node:path";
-import * as dotenv from "dotenv";
-import fp from "fastify-plugin";
+import process from 'node:process';
+import path from 'node:path';
+import * as dotenv from 'dotenv';
+import fp from 'fastify-plugin';
 
 export interface AppConfig {
-  openaiApiUrl: string;
-  openaiEmbeddingDeploymentId: string;
-  openaiChatDeploymentId: string;
+  azureStorageAccount: string;
+  azureStorageContainer: string;
+  azureSearchService: string;
+  azureSearchIndex: string;
+  azureOpenAiService: string;
+  azureOpenAiChatGptDeployment: string;
+  azureOpenAiChatGptModel: string;
+  azureOpenAiEmbDeployment: string;
+  kbFieldsContent: string;
+  kbFieldsSourcePage: string;
 }
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
+const camelCaseToUpperSnakeCase = (str: string) => str.replaceAll(/[A-Z]/g, (l) => `_${l}`).toUpperCase();
+
 export default fp(
   async (fastify, opts) => {
-    const envPath = path.resolve(process.cwd(), "../../.env");
+    const envPath = path.resolve(process.cwd(), '../../.env');
 
     console.log(`Loading .env config from ${envPath}...`);
     dotenv.config({ path: envPath });
 
     const config: AppConfig = {
-      openaiApiUrl: process.env.AZURE_OPENAI_API_URL || "",
-      openaiEmbeddingDeploymentId: process.env.AZURE_OPENAI_EMBEDDING_ID || "text-embedding-ada-002",
-      openaiChatDeploymentId: process.env.AZURE_OPENAI_CHATGPT_ID || "gpt-35-turbo",
+      azureStorageAccount: process.env.AZURE_STORAGE_ACCOUNT || '',
+      azureStorageContainer: process.env.AZURE_STORAGE_CONTAINER || '',
+      azureSearchService: process.env.AZURE_SEARCH_SERVICE || '',
+      azureSearchIndex: process.env.AZURE_SEARCH_INDEX || '',
+      azureOpenAiService: process.env.AZURE_OPENAI_SERVICE || '',
+      azureOpenAiChatGptDeployment: process.env.AZURE_OPENAI_CHATGPT_DEPLOYMENT || '',
+      azureOpenAiChatGptModel: process.env.AZURE_OPENAI_CHATGPT_MODEL || 'gpt-35-turbo',
+      azureOpenAiEmbDeployment: process.env.AZURE_OPENAI_EMB_DEPLOYMENT || '',
+      kbFieldsContent: process.env.KB_FIELDS_CONTENT || 'content',
+      kbFieldsSourcePage: process.env.KB_FIELDS_SOURCEPAGE || 'sourcepage',
     };
 
-    // if (!config.openaiApiUrl) {
-    //   const message = `OPENAI_API_URL is missing!`;
-    //   fastify.log.error(message);
-    //   throw new Error(message);
-    // }
+    // Check that all config values are set
+    for (const [key, value] of Object.entries(config)) {
+      if (!value) {
+        const message = `${camelCaseToUpperSnakeCase(key)} environment variable must be set`;
+        fastify.log.error(message);
+        throw new Error(message);
+      }
+    }
 
-    fastify.decorate("config", config);
+    fastify.decorate('config', config);
   },
   {
-    name: "config",
+    name: 'config',
   },
 );
 
 // When using .decorate you have to specify added properties for Typescript
-declare module "fastify" {
+declare module 'fastify' {
   export interface FastifyInstance {
     config: AppConfig;
   }
