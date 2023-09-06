3e99dc2635c03eaf64f4f6be8dad49a3fa5c49a1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-09-06 15:57:39 -0300

    chore: start the migration from 'openai.ts' to 'azure-openai.ts' file

---
+++ packages/api/src/plugins/azure-openai.ts
@@ -0,0 +1,38 @@
+import fp from 'fastify-plugin';
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
+
+const AZURE_OPENAI_API_VERSION = '2023-05-15';
+const AZURE_COGNITIVE_SERVICES_AD_SCOPE = 'https://cognitiveservices.azure.com/.default';
+
+export default fp(
+  async (fastify, opts) => {
+    const config = fastify.config;
+    const azureOpenAiEndpoint = `https://${config.azureOpenAiService}.openai.azure.com`;
+
+    fastify.log.info(`Using Azure OpenAI at ${azureOpenAiEndpoint}`);
+
+    const azureApiKey = '<include-azure-open-ai-key>';
+
+    const azureOpenAiClient = new OpenAIClient(azureOpenAiEndpoint, new AzureKeyCredential(azureApiKey));
+
+    const refreshOpenAiToken = async () => {
+      // TODO: Implement token refresh
+    };
+
+    azureOpenAiClient.getChatCompletions();
+    azureOpenAiClient.getEmbeddings();
+
+    fastify.decorate('azureOpenAi', {
+      config: {
+        apiVersion: AZURE_OPENAI_API_VERSION,
+        apiUrl: azureOpenAiEndpoint,
+      },
+    });
+  },
+  {
+    name: 'azureopenai',
+    dependencies: ['azure', 'config'],
+  },
+);
+
+declare module 'fastify';
