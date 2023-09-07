0c3e8f4dc71e7cef700d9c0915ebf798c19ea630
sinedied <noda@free.fr>
2023-09-07 15:00:25 +0200

    feat: add blob storage handler

---
+++ packages/ingestion/src/lib/blob-storage.ts
@@ -0,0 +1,57 @@
+import path from 'node:path';
+import { BaseLogger } from 'pino';
+import { AzureClients } from '../plugins/azure';
+
+export class BlobStorage {
+  constructor(
+    private azure: AzureClients,
+    private logger: BaseLogger,
+  ) {}
+
+  async upload(filename: string, data: Buffer, type: string) {
+    const blobContainer = this.azure.blobContainer;
+    await blobContainer.createIfNotExists();
+
+    const blobName = getBlobNameFromFile(filename);
+    const blockBlobClient = blobContainer.getBlockBlobClient(blobName);
+
+    this.logger.debug(`Uploading blob: ${blobName}...`);
+
+    await blockBlobClient.uploadData(data, { blobHTTPHeaders: { blobContentType: type } });
+  }
+
+  async delete(filename: string) {
+    const blobContainer = this.azure.blobContainer;
+    if (await blobContainer.exists()) {
+      if (!filename) {
+        throw new Error('Filename is required');
+      } else {
+        const name = getBlobNameFromFile(filename);
+        const re = new RegExp(`${name}`, 'i');
+
+        const allBlobs = await blobContainer.listBlobsFlat({ prefix: name });
+        for await (const blob of allBlobs) {
+          if (re.test(blob.name)) {
+            this.logger.debug(`Removing blob: ${blob.name}...`);
+            await blobContainer.deleteBlob(blob.name);
+          }
+        }
+      }
+    }
+  }
+
+  async deleteAll() {
+    const blobContainer = this.azure.blobContainer;
+    if (await blobContainer.exists()) {
+      const allBlobs = await blobContainer.listBlobsFlat();
+      for await (const blob of allBlobs) {
+        this.logger.debug(`Removing blob: ${blob.name}...`);
+        await blobContainer.deleteBlob(blob.name);
+      }
+    }
+  }
+}
+
+export function getBlobNameFromFile(filename: string): string {
+  return path.basename(filename);
+}
