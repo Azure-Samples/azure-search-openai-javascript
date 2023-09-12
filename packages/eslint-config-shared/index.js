5e6ec4ef74afc2ceee25a688098891fc1d8dd9f5
sinedied <noda@free.fr>
2023-09-12 14:27:02 +0200

    feat: add eslint rules for react

--- packages/eslint-config-shared/index.js
+++ packages/eslint-config-shared/index.js
@@ -1,9 +1,22 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es2021: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:unicorn/recommended',
     'plugin:n/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
@@ -13,6 +26,9 @@ module.exports = {
       typescript: {
         alwaysTryTypes: true,
       },
+    },
+    react: {
+      version: 'detect',
     },
   },
   root: true,
@@ -34,5 +50,37 @@ module.exports = {
     'n/no-extraneous-import': 'off',
     'n/no-missing-import': 'off',
     'unicorn/prefer-at': 'off',
+    'unicorn/prefer-query-selector': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        allowList: {
+          Props: true,
+          i: true,
+        },
+      },
+    ],
   },
+  overrides: [
+    {
+      files: '*.tsx',
+      rules: {
+        'unicorn/no-useless-undefined': 'off',
+        'unicorn/filename-case': [
+          'error',
+          {
+            case: 'pascalCase',
+          },
+        ],
+      },
+    },
+    {
+      files: 'vite-env.d.ts',
+      rules: {
+        'unicorn/prevent-abbreviations': 'off',
+      },
+    },
+  ],
 };
