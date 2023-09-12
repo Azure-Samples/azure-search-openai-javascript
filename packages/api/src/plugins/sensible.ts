cf006080c5bb3281c12911414867145ff078eb55
sinedied <noda@free.fr>
2023-09-12 11:49:04 +0200

    feat: add shared eslint config

--- packages/api/src/plugins/sensible.ts
+++ packages/api/src/plugins/sensible.ts
@@ -3,7 +3,6 @@ import sensible, { type SensibleOptions } from '@fastify/sensible';
 
 /**
  * This plugins adds some utilities to handle http errors
- *
  * @see https://github.com/fastify/fastify-sensible
  */
 export default fp<SensibleOptions>(async (fastify) => {
