1f5214981e9ddbacd6a553e606ecc973d62fbb45
sinedied <noda@free.fr>
2023-09-13 11:01:07 +0200

    fix(webapp): update output dir

--- packages/webapp/vite.config.ts
+++ packages/webapp/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../../app/backend/static',
+    outDir: './dist',
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
