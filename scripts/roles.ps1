46cce1cceb833c991b28feda2021eefae493a299
sinedied <noda@free.fr>
2023-09-14 10:52:10 +0200

    fix(infra): add missing roles and removed uneeded roles

--- scripts/roles.ps1
+++ scripts/roles.ps1
@@ -10,9 +10,7 @@ Write-Host "Environment variables set."
 
 $roles = @(
     "5e0bd9bd-7b93-4f28-af87-19fc36ad61bd",
-    "2a2b9908-6ea1-4ae2-8e65-a410df84e7d1",
     "ba92f5b4-2d11-453d-a403-e96b0029c9fe",
-    "1407120a-92aa-4202-b7e9-c0e197c71c8f",
     "8ebe5a00-799e-43f5-93ac-243d3dce84a7"
 )
 
