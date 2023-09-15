d204dbbf7da958560bd7f384c22da7bb2685707f
sinedied <noda@free.fr>
2023-09-15 15:27:25 +0200

    feat(indexer): add openapi schema and ui (#35)

--- packages/indexer/src/plugins/multipart.ts
+++ packages/indexer/src/plugins/multipart.ts
@@ -6,7 +6,7 @@ const FILE_UPLOAD_LIMIT = 20 * 1024 * 1024; // 20 MB
 export default fp(async (fastify) => {
   fastify.register(multipart, {
     attachFieldsToBody: true,
-    sharedSchemaId: '#multipartField',
+    sharedSchemaId: 'multipartField',
     limits: {
       fileSize: FILE_UPLOAD_LIMIT,
       files: 1,
