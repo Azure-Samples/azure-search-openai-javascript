5cf0914b1558a89031803a8c34e9144d172bd630
Natalia Venditto <nataliafrontend@gmail.com>
2023-09-15 19:16:56 +0200

    chore: change prompts and rename event

--- packages/frontend-app/src/config/global-config.js
+++ packages/frontend-app/src/config/global-config.js
@@ -2,10 +2,11 @@ export const globalConfig = {
   // Is default prompts enabled?
   IS_DEFAULT_PROMPTS_ENABLED: true,
   // Default prompts to display in the chat
+  DISPLAY_DEFAULT_PROMPTS_BUTTON: 'Not sure what to ask? Try our suggestions!',
   DEFAULT_PROMPTS: [
-    'Show me some relevant information',
-    'Give me the answer I seek!',
-    'Explain me all important details',
+    'How to search and book rentals',
+    'How to cancel a confirmed booking',
+    'How to report a payment or refund issue, for example if a guest or host is asking to pay or be paid outside of the Contoso Real Estate platform',
   ],
   DEFAULT_PROMPTS_HEADING: 'Start a conversation with our support team.',
   // This are the chat bubbles that will be displayed in the chat
@@ -16,6 +17,7 @@ export const globalConfig = {
   CHAT_INPUT_PLACEHOLDER: 'Type your question here',
   USER_IS_BOT: 'Support Bot',
   RESET_BUTTON_LABEL_TEXT: 'X',
+  LOADING_INDICATOR_TEXT: 'Please wait. We are searching for an answer...',
   // API URL for development purposes
   API_URL_LOCAL: '/api/responses',
   API_CHAT_URL: 'http://localhost:3000/chat',
