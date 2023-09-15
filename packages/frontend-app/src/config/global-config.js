f33c32f209d123752b89e3c879b110e09304b056
Natalia Venditto <nataliafrontend@gmail.com>
2023-09-15 14:58:02 +0200

    chore: rename config to js

---
+++ packages/frontend-app/src/config/global-config.js
@@ -0,0 +1,22 @@
+export const globalConfig = {
+  // Is default prompts enabled?
+  IS_DEFAULT_PROMPTS_ENABLED: true,
+  // Default prompts to display in the chat
+  DEFAULT_PROMPTS: [
+    'Show me some relevant information',
+    'Give me the answer I seek!',
+    'Explain me all important details',
+  ],
+  DEFAULT_PROMPTS_HEADING: 'Start chatting, now!',
+  // This are the chat bubbles that will be displayed in the chat
+  CHAT_MESSAGES: [],
+  // This are the labels for the chat button and input
+  CHAT_BUTTON_LABEL_TEXT: 'Ask Support',
+  CHAT_INPUT_LABEL_TEXT: 'Ask a question now',
+  CHAT_INPUT_PLACEHOLDER: 'Type your question here',
+  RESET_BUTTON_LABEL_TEXT: 'Reset',
+  USER_IS_BOT: 'Support Bot',
+  // API URL for development purposes
+  API_URL_LOCAL: '/api/responses',
+  API_CHAT_URL: 'http://localhost:3000/chat',
+};
