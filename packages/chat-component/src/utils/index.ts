bb519c8c9a3b1bc9cb494e8a1b921e75b40afc52
Natalia Venditto <nataliafrontend@gmail.com>
2023-09-24 18:13:45 +0200

    chore: remove brackets from merge conflict resolution

--- packages/chat-component/src/utils/index.ts
+++ packages/chat-component/src/utils/index.ts
@@ -14,7 +14,7 @@ export function processText(inputText: string, arrays: any[][]): ProcessTextRetu
   // Keeping all the regex at this level so they can be easily changed or removed
   const nextQuestionIndicator = NEXT_QUESTION_INDICATOR;
   const findCitations = /\[(.*?)]/g;
-  const findFollowingSteps = /steps:(.*?)(?:Next Questions:|<<|$)/s;
+  const findFollowingSteps = `/steps:(.*?)(?:Next Questions:|<<|$)/s`;
   const findNextQuestions = /Next Questions:(.*?)$/s;
   const findQuestionsbyDoubleArrow = /<<([^<>]+)>>/g;
   const findNumberedItems = /\d+\.\s+/;
