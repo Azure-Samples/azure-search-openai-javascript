46369774c66dc49ece9434c8ed1e0791b10fa7d8
Natalia Venditto <nataliafrontend@gmail.com>
2023-09-27 13:05:58 +0200

    feat: add streaming frontend

--- packages/chat-component/src/types.d.ts
+++ packages/chat-component/src/types.d.ts
@@ -18,3 +18,23 @@ declare interface ProcessTextReturn {
   replacedText: string;
   arrays: Array<Array<string> | Array<Citation>>;
 }
+
+declare interface RequestOptions {
+  method: string;
+  approach: string[];
+  stream?: boolean;
+  overrides: RequestOverrides;
+}
+
+declare interface RequestOverrides {
+  retrieval_mode?: string;
+  semantic_ranker?: boolean;
+  semantic_captions?: boolean;
+  top?: number;
+  suggest_followup_questions?: boolean;
+  temperature?: number;
+  prompt_template?: string;
+  prompt_template_prefix?: string;
+  prompt_template_suffix?: string;
+  exclude_category?: string[];
+}
