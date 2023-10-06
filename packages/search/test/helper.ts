2b68b0230122d71ba4837442a3f7bf25c10c4570
sinedied <noda@free.fr>
2023-10-06 11:11:09 +0200

    test: add api snapshot test

--- packages/search/test/helper.ts
+++ packages/search/test/helper.ts
@@ -10,7 +10,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export type Test = (typeof tap)['Test']['prototype'];
 
-const AppPath = path.join(__dirname, '..', 'src', 'app.js');
+const APP_PATH = path.join(__dirname, '../src/app.js');
+const ENV_PATH = path.join(__dirname, '../../../../.env');
 
 // Fill in this config with all the configurations
 // needed for testing the application
@@ -18,22 +19,33 @@ async function config() {
   // Copy package.json to the test directory
   await fs.copyFile(path.join(__dirname, '../../package.json'), path.join(__dirname, '../../test-dist/package.json'));
 
+  // Use dummy values when .env file is not present
   process.env.AZURE_OPENAI_CHATGPT_DEPLOYMENT = 'chat';
   process.env.AZURE_OPENAI_CHATGPT_MODEL = 'gpt-3.5-turbo';
   process.env.AZURE_OPENAI_EMBEDDING_DEPLOYMENT = 'embeddings';
   process.env.AZURE_OPENAI_EMBEDDING_MODEL = '';
   process.env.AZURE_OPENAI_SERVICE = 'https://example.com';
-  process.env.AZURE_SEARCH_INDEX = 'testindex';
+  process.env.AZURE_SEARCH_INDEX = 'gptkbindex';
   process.env.AZURE_SEARCH_SERVICE = 'https://example.com';
   process.env.AZURE_STORAGE_ACCOUNT = 'dummystorage';
-  process.env.AZURE_STORAGE_CONTAINER = 'testfiles';
+  process.env.AZURE_STORAGE_CONTAINER = 'content';
+
+  if (process.env.TAP_SNAPSHOT) {
+    try {
+      // Check if .env file exists
+      await fs.access(ENV_PATH);
+    } catch (e) {
+      throw new Error(`.env file not found at ${ENV_PATH}: required to update snapshots`);
+    }
+  }
+
   return {};
 }
 
 // Automatically build and tear down our instance
 async function build(t: Test) {
   // you can set all the options supported by the fastify CLI command
-  const argv = [AppPath];
+  const argv = [APP_PATH];
 
   // fastify-plugin ensures that all decorators
   // are exposed for testing purposes, this is
--- packages/search/test/routes/root.test.ts
+++ packages/search/test/routes/root.test.ts
@@ -3,11 +3,34 @@ import { build } from '../helper.js';
 
 t.test('default root route', async (t) => {
   const app = await build(t);
-
   const response = await app.inject({
     url: '/',
   });
 
   const result = JSON.parse(response.payload);
   t.hasProps(result, ['service', 'description', 'version']);
+});
+
+t.test('/chat without streaming', async (t) => {
+  t.nock.snapshot();
+
+  const app = await build(t);
+  const response = await app.inject({
+    url: '/chat',
+    method: 'POST',
+    payload: {
+      history: [{ user: "What happens if a rental doesn't fit the description?" }],
+      approach: 'rrr',
+      overrides: {
+        retrieval_mode: 'hybrid',
+        semantic_ranker: true,
+        semantic_captions: false,
+        top: 3,
+        suggest_followup_questions: false,
+      },
+    },
+  });
+
+  const result = JSON.parse(response.payload);
+  t.match(result, {});
 });
