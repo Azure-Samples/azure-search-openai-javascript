1af860136500d331d0d61d67215549ef78f324b9
Yohan Lasorsa <noda@free.fr>
2023-10-10 07:50:35 +0000

    chore: add logs for allowed origins

--- packages/search/src/plugins/cors.ts
+++ packages/search/src/plugins/cors.ts
@@ -4,6 +4,7 @@ import cors from '@fastify/cors';
 export default fp(
   async (fastify) => {
     const allowedOrigins = fastify.config.allowedOrigins.split(',').map((origin) => origin.trim());
+    fastify.log.info(`CORS allowed origins: ${allowedOrigins.join(', ')}`);
     fastify.register(cors, {
       origin: allowedOrigins,
     });
