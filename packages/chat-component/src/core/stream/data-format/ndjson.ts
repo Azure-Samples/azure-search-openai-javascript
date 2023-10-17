8c8459621b5edb94f8ed520823ec2b1cd64b398e
Yohan Lasorsa <noda@free.fr>
2023-10-17 10:07:12 +0000

    fix(chat): streaming issue

--- packages/chat-component/src/core/stream/data-format/ndjson.ts
+++ packages/chat-component/src/core/stream/data-format/ndjson.ts
@@ -1,4 +1,5 @@
 export class NdJsonParserStream extends TransformStream<string, JSON> {
+  private buffer: string = '';
   constructor() {
     let controller: TransformStreamDefaultController<JSON>;
     super({
@@ -8,7 +9,13 @@ export class NdJsonParserStream extends TransformStream<string, JSON> {
       transform: (chunk) => {
         const jsonChunks = chunk.split('\n').filter(Boolean);
         for (const jsonChunk of jsonChunks) {
-          controller.enqueue(JSON.parse(jsonChunk));
+          try {
+            this.buffer += jsonChunk;
+            controller.enqueue(JSON.parse(this.buffer));
+            this.buffer = '';
+          } catch {
+            // Invalid JSON, wait for next chunk
+          }
         }
       },
     });
