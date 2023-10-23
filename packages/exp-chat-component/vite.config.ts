7bdead97f2a54bcfaa1341672551fe2ed30d9b7b
sinedied <noda@free.fr>
2023-10-23 17:56:37 +0200

    chore(chat): export api url

--- packages/exp-chat-component/vite.config.ts
+++ packages/exp-chat-component/vite.config.ts
@@ -1,6 +1,10 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
+// Expose environment variables to the client
+process.env.VITE_CHAT_API_URI = process.env.CHAT_API_URI ?? '';
+console.log(`Using search API base URL: "${process.env.VITE_CHAT_API_URI}"`);
+
 export default defineConfig({
   build: {
     emptyOutDir: true,
