d61f6debf50d61f8a6f4a6301417c8003d78dee9
sinedied <noda@free.fr>
2023-10-26 18:40:50 +0200

    fix(chat): use proxy by default to allow working in codespaces

--- packages/exp-chat-component/src/api.ts
+++ packages/exp-chat-component/src/api.ts
@@ -1,6 +1,6 @@
 import { type ChatResponse, type ChatRequestOptions, type ChatResponseChunk } from './models.js';
 
-export const apiBaseUrl = import.meta.env.VITE_CHAT_API_URI || 'http://localhost:3000';
+export const apiBaseUrl = import.meta.env.VITE_CHAT_API_URI || '';
 
 export async function getCompletion(options: ChatRequestOptions, oneShot = false) {
   const response = await fetch(`${apiBaseUrl}/${oneShot ? 'ask' : 'chat'}`, {
