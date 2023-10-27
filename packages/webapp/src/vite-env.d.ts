659e9993523fdf244ba4129a73d5dda862a5e842
sinedied <noda@free.fr>
2023-10-27 14:54:11 +0200

    chore(webapp): fix ask page

--- packages/webapp/src/vite-env.d.ts
+++ packages/webapp/src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-import { type DOMAttributes } from 'react';
+import type React, { type DOMAttributes } from 'react';
 import { type ChatComponent } from '@azure/chat';
 
 type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any } & HTMLElement & HTMLAttributes<HTMLElement>>;
@@ -9,6 +9,7 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       ['azc-chat']: CustomElement<ChatComponent>;
+      ['chat-component']: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
     }
   }
 }
