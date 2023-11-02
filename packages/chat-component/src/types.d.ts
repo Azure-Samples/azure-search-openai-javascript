2f78d477f004bcdc9ce6e2d4d0bb1d651fb1909b
Natalia Venditto <nataliafrontend@gmail.com>
2023-11-02 19:07:13 +0100

    chore: add a teaser type

--- packages/chat-component/src/types.d.ts
+++ packages/chat-component/src/types.d.ts
@@ -32,6 +32,12 @@ declare interface ProcessTextReturn {
   arrays: Array<Array<string> | Array<Citation>>;
 }
 
+declare interface Teaser {
+  title?: string;
+  description: string;
+  icon?: string;
+}
+
 declare interface ChatRequestOptions {
   approach: string;
   overrides: RequestOverrides;
