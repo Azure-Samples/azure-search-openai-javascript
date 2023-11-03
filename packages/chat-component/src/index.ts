8da1949c5c3ed8c052ff04766299193692657090
Natalia Venditto <nataliafrontend@gmail.com>
2023-11-03 21:59:46 +0100

    chore: add tab component to bundle

--- packages/chat-component/src/index.ts
+++ packages/chat-component/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/teaserlist-component.js';
+export * from './components/tab-component.js';
 export * from './components/chat-component.js';
 export * from './core/index.js';
 export * from './utils/index.js';
