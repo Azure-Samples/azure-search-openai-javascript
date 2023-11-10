44cab41b0b423a3616386a69b0fdd83d6962a35a
Natalia Venditto <nataliafrontend@gmail.com>
2023-11-10 15:51:02 +0100

    chore: update styles for teaserlist

--- packages/chat-component/src/styles/chat-component.ts
+++ packages/chat-component/src/styles/chat-component.ts
@@ -474,56 +474,6 @@ export const chatStyle = css`
     border-bottom: 1px solid var(--light-gray);
     font-size: small;
   }
-  .defaults__button {
-    text-decoration: none;
-    color: var(--text-color);
-    display: block;
-    font-size: 1.2rem;
-  }
-  .defaults__list {
-    list-style-type: none;
-    padding: 0;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-  .defaults__list.always-row {
-    text-align: left;
-  }
-  .defaults__list:not(.always-row) {
-    @media (min-width: 1024px) {
-      flex-direction: row;
-    }
-  }
-  .defaults__listItem {
-    padding: 10px;
-    border-radius: 10px;
-    background: var(--white);
-    margin: 4px;
-    color: var(--text-color);
-    justify-content: space-evenly;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    border: 3px solid transparent;
-
-    @media (min-width: 768px) {
-      min-height: 100px;
-    }
-  }
-  .defaults__listItem:hover,
-  .defaults__listItem:focus {
-    color: var(--accent-dark);
-    background: var(--secondary-color);
-    transition: all 0.3s ease-in-out;
-    border-color: var(--accent-high);
-  }
-  .defaults__span {
-    color: var(--accent-high);
-    font-weight: bold;
-    display: block;
-    margin-top: 20px;
-    text-decoration: underline;
-  }
   .citation {
     background-color: var(--accent-lighter);
     border-radius: 3px;
