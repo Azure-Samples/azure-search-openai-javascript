a16306e8159f838ab72df997788d8eeae7126f75
sinedied <noda@free.fr>
2023-11-16 11:01:08 +0100

    fix(indexer): incorrect path on Windows (#141)

--- packages/indexer/bin/index-files.js
+++ packages/indexer/bin/index-files.js
@@ -1,21 +1,5 @@
 #!/usr/bin/env node
 import process from 'node:process';
-import { execSync } from 'node:child_process';
 import { run } from '../dist/lib/cli.js';
 
-if ((process.env.NODE_OPTIONS ?? '').includes('--no-warnings')) {
-  run(process.argv);
-} else {
-  // Silence experimental warnings
-  try {
-    execSync(process.argv.join(' '), {
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        NODE_OPTIONS: `${process.env.NODE_OPTIONS ?? ''} --no-warnings`,
-      },
-    });
-  } catch {
-    // Do nothing
-  }
-}
+run(process.argv);
