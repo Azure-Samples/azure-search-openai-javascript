73f312e98ad63de9d38d412f4ca9f360da8bbf3b
Natalia Venditto <nataliafrontend@gmail.com>
2023-11-27 09:58:23 +0100

    feat: install chat component

--- packages/webapp/src/app-settings/app-settings.component.ts
+++ packages/webapp/src/app-settings/app-settings.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import 'chat-component';
 
 @Component({
   selector: 'app-app-settings',
@@ -11,8 +12,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
   imports: [CommonModule, MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule],
   templateUrl: './app-settings.component.html',
   styleUrl: './app-settings.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppSettingsComponent {
   events: string[] = [];
   opened: boolean = false;
+  panelLabel: string = 'App Settings';
 }
