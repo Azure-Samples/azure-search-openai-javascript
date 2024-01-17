2bdf72cca3bc8f40aa9a605721ba04c235fcacfd
Natalia Venditto <nataliafrontend@gmail.com>
2024-01-17 20:03:00 +0100

    chore: add azure b2c

--- packages/webapp/src/app/api/auth/[...nextauth]/options.ts
+++ packages/webapp/src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,20 @@
 import NextAuth, { type AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import AzureADB2CProvider from 'next-auth/providers/azure-ad-b2c';
 
 export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // we will need to write the docs for this to work
+    // https://next-auth.js.org/providers/azure-ad-b2c
+    // TODO: update docs of next-auth with EntraID
+    AzureADB2CProvider({
+      clientId: process.env.AZUREADB2C_CLIENT_ID as string,
+      clientSecret: process.env.AZUREADB2C_CLIENT_SECRET as string,
+      tenantId: process.env.AZUREADB2C_TENANT_ID as string,
     }),
   ],
 };
