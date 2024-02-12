e16dc155554de4bcf75f443456fefa618539e552
Shibani Basava <Shibani.Basava@microsoft.com>
2024-02-12 10:59:32 +0000

    feat: move features to their own file to compose on demand

---
+++ packages/chat-component/src/components/features.ts
@@ -0,0 +1,10 @@
+// [COMPOSE COMPONENTS START]
+import './voice-input.js';
+import './voice-input-button.js';
+
+import './default-questions.js';
+import './teaser-list-component.js';
+
+import './document-previewer.js';
+import './citation-previewer.js';
+// [COMPOSE COMPONENTS END]
--- packages/chat-component/src/components/index.ts
+++ packages/chat-component/src/components/index.ts
@@ -6,18 +6,7 @@ import './tab-component.js';
 import './citation-list.js';
 import './chat-thread-component.js';
 import './chat-action-button.js';
-
 import './copy-entry-to-clipboard.js';
 
-// [COMPOSE COMPONENTS START]
-import './voice-input.js';
-import './voice-input-button.js';
-
-import './default-questions.js';
-import './teaser-list-component.js';
-
-import './document-previewer.js';
-import './citation-previewer.js';
-// [COMPOSE COMPONENTS END]
-
+import './features.js';
 export * from './chat-component.js';
